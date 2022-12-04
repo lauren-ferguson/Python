a7b0d3ad72815157451bbbfa89cf70855b459f1b
Lauren Ferguson <113648932+lauren-ferguson@users.noreply.github.com>
2022-12-03 22:14:19 -0600

    Update Random IP Gen with User Input for Subnet.py

--- Random IP Gen with User Input for Subnet.py
+++ Random IP Gen with User Input for Subnet.py
@@ -1,10 +1,10 @@
 # IP Gen w/Subnet Input
 # Input Subnet
-shift = 0
-while not int(shift) in range(1, 36):
-    shift = input("Please enter your subnet (1 - 35) : ")  # choose a subnet
+subnet = 0
+while not int(subnet) in range(1, 36):
+    subnet = input("Please enter your subnet (1 - 35) : ")  # choose a subnet
     
 # IP Gen, 2 Outputs
 import random
 for i in range(2):
-    print(".".join(str(random.randint(0, 255)) for _ in range(4)) + "/" + shift)
+    print(".".join(str(random.randint(0, 255)) for _ in range(4)) + "/" + subnet)
